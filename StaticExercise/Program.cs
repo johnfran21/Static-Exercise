42b1a2020f28d80339356f64c53ef7a5072aa4df
John Franzese <johnfranzese21@gmail.com>
2024-02-21 15:44:29 -0600

    Static Updates

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double fahrenheitValue = 98.6;
+            double celsiusValue = 37;
+
+            // Convert Fahrenheit to Celsius
+            double celsiusResult = TempConverter.FahrenheitToCelsius(fahrenheitValue);
+            Console.WriteLine($"{fahrenheitValue} Fahrenheit is {celsiusResult:F2} Celsius");
+
+            // Convert Celsius to Fahrenheit
+            double fahrenheitResult = TempConverter.CelsiusToFahrenheit(celsiusValue);
+            Console.WriteLine($"{celsiusValue} Celsius is {fahrenheitResult:F2} Fahrenheit");
         }
+    
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+        
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            // Fahrenheit formula: (F - 32) * 5/9
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            // Celsius formula: (C * 9/5) + 32
+            return (celsius * 9 / 5) + 32;
+        }
+
+    }
+}
+
